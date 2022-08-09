4443598b42b3727edc63c74d0e523252e27bfb29
Edgar Alexandro <edgar@MacBook-Pro-de-Edgar.local>
2022-08-09 07:37:14 -0500

    app01

---
+++ App01-MiPerfil/App01-MiPerfil/App01_MiPerfilApp.swift
@@ -0,0 +1,17 @@
+//
+//  App01_MiPerfilApp.swift
+//  App01-MiPerfil
+//
+//  Created by Edgar Alexandro on 08/08/22.
+//
+
+import SwiftUI
+
+@main
+struct App01_MiPerfilApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ App01-MiPerfil/App01-MiPerfil/HomeView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  App01-MiPerfil
+//
+//  Created by Edgar Alexandro on 08/08/22.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
