f25a108da7190ea611d7972bb9f76d87ec398550
Edgar Alexandro <edgar@MacBook-Pro-de-Edgar.local>
2022-08-11 11:57:07 -0500

    app01

--- Apps/App01-MiPerfil/App01-MiPerfil/App01_MiPerfilApp.swift
+++ Apps/App01-MiPerfil/App01-MiPerfil/App01_MiPerfilApp.swift
@@ -2,7 +2,7 @@
 //  App01_MiPerfilApp.swift
 //  App01-MiPerfil
 //
-//  Created by Edgar Alexandro on 08/08/22.
+//  Created by Edgar Alexandro on 11/08/22.
 //
 
 import SwiftUI
@@ -11,7 +11,7 @@ import SwiftUI
 struct App01_MiPerfilApp: App {
     var body: some Scene {
         WindowGroup {
-            HomeView()
+            ContentView()
         }
     }
 }
