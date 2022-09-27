d19cf100bf80515245c9f58ef7c11215a8398a32
Edgar Alexandro Castillo Palacios <a00830568@tec.mx>
2022-09-26 19:34:18 -0500

    App08

--- Apps/App08-PAM/App08-PAM/ContentView.swift
+++ Apps/App08-PAM/App08-PAM/ContentView.swift
@@ -11,15 +11,55 @@ struct ContentView: View {
     
     @StateObject var pamModel = PAMModel()
     
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+    var dateFormat: DateFormatter{
+        let formatter = DateFormatter()
+        formatter.dateFormat="yyyy/MMM/dd"
+        return formatter
     }
+    
+    var body: some View{
+        NavigationView{
+            ZStack{
+                List{
+                    ForEach(pamModel.pams) { pam in
+                        VStack {
+                            Text(pam.name)
+                            Text(dateFormat.string(from:pam.birth_date))
+                        }
+                    }
+                    .onDelete(perform: deletePAM)
+                }
+                .toolbar{
+                    ToolbarItem(placement: .navigationBarTrailing){
+                        EditButton()
+                    }
+                }
+                Button {
+                    addPAM()
+                } label: {
+                    Image(systemName: "plus")
+                        .font(.largeTitle)
+                }
+                .padding()
+                .background(Color.green)
+                .foregroundColor(.white)
+                .clipShape(Circle())
+            }
+        }
+    }
+    
+    func deletePAM(offsets: IndexSet){
+        for index in offsets{
+            pamModel.deletePAM(pam: pamModel.pams[index])
+            pamModel.pams.remove(at: index)
+        }
+    }
+    
+    func addPAM(){
+        var pam = PAM(id: 0, name: "Brenda", birth_date: Date(), gender: "Mujer")
+        pamModel.addPAM(pam: pam)
+    }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
--- Apps/App08-PAM/App08-PAM/Models/PAMModel.swift
+++ Apps/App08-PAM/App08-PAM/Models/PAMModel.swift
@@ -18,18 +18,43 @@ class PAMModel: ObservableObject{
     }
     
     func loadPAMs(){
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSZ"
         pams.removeAll()
         let URL = "http://10.14.255.61:3000/api/getPams"
         
         AF.request(URL, method: .get, encoding: URLEncoding.default).responseData { data in
             let json = try! JSON(data: data.data!)
-            print(json)
-//            for genre in json["genres"] {
-//                let g = Genre(id: genre.1["id"].intValue,
-//                              name: genre.1["name"].stringValue
-//                )
-//                self.genresMovies.append(g)
-//            }
+            //            print(json)
+            var auxPam: PAM
+            for pam in json{
+                auxPam = PAM(
+                    id: pam.1["pam_id"].intValue,
+                    name:pam.1["name"].stringValue,
+                    birth_date:formatter.date(from:pam.1["birth_day"].stringValue)!,
+                    gender:pam.1["gender"].stringValue)
+                self.pams.append(auxPam)
+            }
+        }
+    }
+    
+    func deletePAM(pam: PAM){
+        let URL = "http://10.14.255.61:3000/api/deletePam/\(pam.id)"
+        AF.request(URL, method: .get, encoding: URLEncoding.default).responseData { data in
+            print(data.data!)
+        }
+    }
+    
+    func addPAM(pam: PAM){
+        let URL = "http://10.14.255.61:3000/api/addPam"
+        var body: [String: Any] = [
+            "name": pam.name,
+            "birth_day": "2001-09-12",
+            "gender": pam.gender
+        ]
+        print(body)
+        AF.request(URL, method: .post, parameters: body, encoding: URLEncoding.default).responseJSON { data in
+            print(data.response)
         }
     }
 }
